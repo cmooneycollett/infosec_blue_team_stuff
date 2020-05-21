f19e20c7ccfe5f271db8b7b14518a79ef170567e
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2020-05-22 09:47:17 +1000

    Added script to find UDP endpoints on computer
    Requires Administrator privilege to run

---
+++ powershell/Get-UDPEndpoints.ps1
@@ -0,0 +1,14 @@
+﻿# Get set of all processes
+$Processes = @{}
+Get-Process -IncludeUserName | % { $Processes.Add($_.Id, $_) }
+
+# Get UDP endpoints
+Get-NetUDPEndpoint |
+    Where-Object {$_.LocalAddress -eq "0.0.0.0"} |
+    Select-Object LocalAddress,
+        LocalPort,
+        @{Name="PID"; Expression={$_.OwningProcess}},
+        @{Name="ProcessName"; Expression={$Processes[[int]$_.OwningProcess].ProcessName}},
+        @{Name="UserName"; Expression={$Processes[[int]$_.OwningProcess].UserName}},
+        @{Name="Path"; Expression={$Processes[[int]$_.OwningProcess].Path}} |
+    Format-Table -AutoSize
