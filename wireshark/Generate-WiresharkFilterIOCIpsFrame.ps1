60cb7f6a167471160b7c58d065ae3fc43fb1810a
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2020-05-29 12:31:16 +1000

    Added PS script to gen filter for IOC IP addresses contained within frames
    - Modified version of the script generating Wireshark display filter with
      "ip.addr == <IP_ADDR>"

---
+++ wireshark/Generate-WiresharkFilterIOCIpsFrame.ps1
@@ -0,0 +1,20 @@
+param (
+    [Parameter(Mandatory=$true)]
+    [string] $in_file
+)
+# Initialise count
+$count = 0
+$display_filter = "("
+# Read each IP address listed in given file
+Get-Content -Path $in_file | ForEach-Object {
+    # Remove any leading or trailing whitespace
+    $ip_addr = $_.Trim()
+    if ($count -eq 0) {
+        $display_filter += [String]::Format("frame contains == ""{0}""", $ip_addr)
+    } else {
+        $display_filter += [String]::Format(" || frame contains == ""{0}""", $ip_addr)
+    }
+    $count += 1
+}
+$display_filter += ")"
+Write-Output $display_filter
