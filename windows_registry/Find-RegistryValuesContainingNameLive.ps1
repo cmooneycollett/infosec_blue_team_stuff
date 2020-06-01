9fa2a46f054bcb90cd2d3b6227144d04a7ed6482
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2020-06-01 19:46:53 +1000

    Tidied up script output formatting

--- windows_registry/Find-RegistryValuesContainingNameLive.ps1
+++ windows_registry/Find-RegistryValuesContainingNameLive.ps1
@@ -3,7 +3,7 @@ param (
     [string] $in_file
 )
 # Open the in-file and read into list
-$target_names = Get-Content -Path $in_file | % {$_.ToLower()}
+$target_names = Get-Content -Path $in_file
 # Initialise registry roots
 $reg_roots = (
     "HKCU:\",
@@ -12,22 +12,28 @@ $reg_roots = (
 )
 # Recurse through the registry hives of interest
 ForEach ($reg_root in $reg_roots) {
-    Get-ChildItem -Path $reg_root -Recurse | % {
+    Get-ChildItem -Path $reg_root -Recurse | ForEach-Object {
         # Get all the sub-values
         $key = $_
+        $key_name_printed = $false
         # Check each of the current key's values
-        $key.GetValueNames() | % {
+        $key.GetValueNames() | ForEach-Object {
             $value_name = $_
             # Get the value held by the value name
             $value = $key.GetValue($value_name)
             # Perform case-insenstive comparison of the registry value against each target name
             ForEach ($target_name in $target_names) {
                 if ($value -match $target_name) {
-                    Write-Output "+ $($key.Name)"
+                    if ($key_name_printed -eq $false) {
+                        Write-Output "+ $($key.Name)"
+                        $key_name_printed = $true
+                    }
                     Write-Output "|-------- $($value_name) _____ $($value)"
-                    Write-Output ""
                 }
             }
+        }
+        if ($key_name_printed) {
+            Write-Output ""
         }
     }
 }
