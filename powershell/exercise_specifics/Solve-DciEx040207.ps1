cfff95d5956be91088d7fd9a1cbb3a96c9928971
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2020-06-11 13:41:04 +1000

    Refined script to look for running keylogger process vice searching disk

--- powershell/exercise_specifics/Solve-DciEx040207.ps1
+++ powershell/exercise_specifics/Solve-DciEx040207.ps1
@@ -1,15 +1,14 @@
-# Q1 - Search through entire C: drive for all files called "KeyX.exe"
-Get-ChildItem -Path "C:\" -Recurse -Force -ErrorAction SilentlyContinue | ForEach-Object {
-    if ($_ -isnot [System.IO.DirectoryInfo]) {
-        if ($_.Name -eq "KeyX.exe") {
-            Write-Host $_.FullName
-        }
-    }
-}
+# Q1 - Find path for the KeyX keylogger process running on the computer
+Write-Host -ForegroundColor Yellow "[?] Q1 - Find path for KeyX keylogger running on computer"
+
+Get-Process | ? {$_.Name -match "KeyX"} | Select-Object -Property Name,Id,Path | Format-Table
+
 # Q2, Q3 - look for *.log files modified a day either side of the known "teeamware.log" file
-$log_file_mod_date = (Get-Item -Path "C:\Users\DCI Student\AppData\Roaming\teeamware.log").LastWriteTime
-$start_date = $log_file_mod_date.AddDays(-1)
-$end_date = $log_file_mod_date.AddDays(1)
+Write-Host -ForegroundColor Yellow "[?] Q2 - Finding *.log files modified within 2 minutes of teeamware.log"
+
+$log_file_mod_date = (Get-Item -Path "C:\Users\Administrator\AppData\Roaming\teeamware.log").LastWriteTime
+$start_date = $log_file_mod_date.AddMinutes(-2)
+$end_date = $log_file_mod_date.AddMinutes(2)
 Get-ChildItem -Path "C:\" -Recurse -Force -Include "*.log" -ErrorAction SilentlyContinue | Where-Object {
     $_.LastWriteTime -ge $start_date -and $_.LastWriteTime -le $end_date
 }
