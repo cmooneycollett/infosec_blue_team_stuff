c62c8a8583f08517125b29687a41b10951c727b6
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2020-06-19 11:52:42 +1000

    Updated snort rule script to also generate alerts for HTTP requests

--- snort/gen_snort_alerts.py
+++ snort/gen_snort_alerts.py
@@ -35,6 +35,7 @@
 ################################################################################
 
 import argparse
+import re
 
 def main():
     # Extract command-line arguments
@@ -42,32 +43,39 @@ def main():
     parser.add_argument('--name', dest='apt_name', action='store', required=True, help='Name of APT')
     parser.add_argument('--ip_file', dest='ip_file', action='store', required=True, help='List of APT IOC IP addresses')
     parser.add_argument('--domain_file', dest='domain_file', action='store', required=True, help='List of APT IOC domains')
+    parser.add_argument('--http_req_file', dest='http_req_file', action='store', required=True, help='List of APT IOC HTTP request filepaths')
     args = parser.parse_args()
     apt_name = args.apt_name
     # Open IOC files
     ip_ioc_file = open(args.ip_file, 'r')
     domain_ioc_file = open(args.domain_file, 'r')
+    http_req_ioc_file = open(args.http_req_file, 'r')
+
     # Initialise alert SID
     sid = 1000001
+
     # Print out the ICMP test rule
     print('# Test rules')
-    print('alert icmp any any <> any any (msg:\"ICMP Test\"; sid: {})'.format(sid))
+    print('# alert icmp any any <> any any (msg:\"ICMP Test\"; sid: {};)'.format(sid))
     sid += 1
     print()
+
     # Print out rules for APT IOC IP addresses
     print('# Rules for {} IOCs - IP addresses'.format(apt_name))
-    bad_ips = ''
     for line in ip_ioc_file.readlines():
         line = line.strip() # Remove leading and trailing whitespace
-        bad_ips += '{},'.format(line)
-    bad_ips = bad_ips[:-1]
-    print('alert ip [{}] any -> any any (msg:\"BADNESS - Detected {} IOC - IP address\"; sid: {};)'.format(bad_ips, apt_name, sid))
+        if len(line) == 0:
+            continue
+        print('alert ip any any -> {} any (msg:\"BADNESS - Detected {} IOC - IP addr - {}\"; sid: {};)'.format(line, apt_name, line, sid))
+        sid += 1
     print()
-    sid += 1
+
     # Print out rules for APT IOC domains
     print('# Rules for {} IOCs - domains'.format(apt_name))
     for line in domain_ioc_file.readlines():
         line = line.strip() # Remove leading and training whitespace
+        if len(line) == 0: # Ignore empty lines
+            continue
         url_elements = line.split('.')
         content_str = ''
         for elem in url_elements:
@@ -78,6 +86,25 @@ def main():
         print('alert udp any any -> any any (msg:\"BADNESS - Detected {} IOC - DNS req for {}\"; {} sid: {};)'.format(apt_name, line, content_str, sid))
         sid += 1
     print()
+
+    # Print out rules for APT HTTP GET requests
+    print('# Rule for {} IOCs - HTTP GET requests'.format(apt_name))
+    for line in http_req_ioc_file.readlines():
+        line = line.strip()
+        if len(line) == 0:
+            continue
+        elements = line.replace('/', '.').split('.')
+        content_str_get = 'content: \"GET\"; http_method;'
+        content_str_post = 'content: \"POST\"; http_method;'
+        for elem in elements:
+            content_str_get += ' content:\"{}\";'.format(elem)
+            content_str_post += ' content:\"{}\";'.format(elem)
+        print('alert tcp any any -> any 80 (msg:\"BADNESS - Detected {} IOC - HTTP GET for file {}\"; {} sid: {};)'.format(apt_name, line, content_str_get, sid))
+        sid += 1
+        print('alert tcp any any -> any 80 (msg:\"BADNESS - Detected {} IOC - HTTP POST for file {}\"; {} sid: {};)'.format(apt_name, line, content_str_post, sid))
+        sid += 1
+    print()
+
     print('# END OF RULES')
 
 if __name__ == '__main__':
